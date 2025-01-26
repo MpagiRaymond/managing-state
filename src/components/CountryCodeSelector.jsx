7b68daf541cbe304cc54b1492ea006ac01d7f71f
MpagiRaymond <raympagi1@gmail.com>
2025-01-26 13:40:07 -0500

    styling react-select component

--- src/components/CountryCodeSelector.jsx
+++ src/components/CountryCodeSelector.jsx
@@ -1,44 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import './../App.css';
 
 
-const CountryCodeSelector = ({selectedCountry, onChange}) => {
-    const countryOptions =[
-        {value: '+256', label: 'Uganda (+256)'},
-        {value: '+254', label: 'kenya (+254)'},
-        {value: '+255', label: 'Tanzania (+255)'},
-        {value: '+250', label: 'Rwanda (+250)'},
+const CountryCodeSelector = ({ selectedCountry, onChange }) => {
+    const countryOptions = [
+        { value: '+256', label: '(+256) Uganda' },
+        { value: '+254', label: '(+254) kenya' },
+        { value: '+255', label: '(+255) Tanzania' },
+        { value: '+250', label: '(+250) Rwanda' },
     ];
 
     const customStyles = {
         option: (provided, state) => ({
           ...provided,
           fontSize: 14,
-          color: 'blue',
+          color: state.isSelected ? 'darkblue' : 'blue',
           backgroundColor: state.isSelected ? 'green' : 'yellow',
         }),
         control: (provided) => ({
           ...provided,
-          width: 200,
-          padding: 10,
+          width: 125,
+          padding: 3.5,
           fontSize: 16,
-          border: '2px solid #4CAF50',
+          border: '2px solid white',
           borderRadius: 5,
           backgroundColor: '#f9f9f9',
           color: '#333'
         }),
+        singleValue: (provided) => ({
+            ...provided,
+            color: 'green', // Change text color to red in the input box
+          }),
       };
 
 return (
-    <Select
-        value={selectedCountry}
-        onChange={onChange}
-        options={countryOptions}
-        placeholder="+91"  
-        style={customStyles}  
-    />
-);
-}
+        <Select
+            value={selectedCountry}
+            onChange={onChange}
+            options={countryOptions}
+            placeholder="(+911)"  
+            styles={customStyles}  
+        />
+    );
+};
  
 export default CountryCodeSelector;
